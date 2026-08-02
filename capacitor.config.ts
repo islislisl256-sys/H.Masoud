b6c871b6485f8e5779e7458748f6d2cf2cf28b34
he is <ish037390@gmail.com>
2026-08-03 00:19:49 +0100

    feat: Add Android platform with Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sahacare.app',
+  appName: 'SahaCare',
+  webDir: 'out'
+};
+
+export default config;
