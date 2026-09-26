a422e9cade709ce57d6101a4525e040e03240b91
he is <ish037390@gmail.com>
2026-09-26 07:09:12 +0100

    Update app with native URL launcher for contact links

---
+++ fix_web_links.js
@@ -0,0 +1,9 @@
+﻿const fs = require('fs');
+
+let content = fs.readFileSync('src/app/contact/page.tsx', 'utf8');
+
+// Completely remove the onClick handler from the anchors
+content = content.replace(/onClick=\{\(e\) => handleLinkClick\(e, '[^']+'\)\}/g, '');
+
+fs.writeFileSync('src/app/contact/page.tsx', content, 'utf8');
+console.log('Removed onClick interceptor from web links');
